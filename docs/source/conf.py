fa80ee539283ce88619797f0681bc0f9d133877a
Devang K <52554056+rising-entropy@users.noreply.github.com>
2022-03-23 21:50:11 +0530

    soem

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Milo'
+copyright = '2022, Devang'
+author = 'Devang'
 
 release = '0.1'
 version = '0.1.0'
